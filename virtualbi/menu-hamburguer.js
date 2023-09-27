07ced4488eff81e695531c8fd78ca0c66308a0a4
Miranda Morais Quimuanga <111236665+mirandaquimuanga@users.noreply.github.com>
2023-09-27 10:51:40 +0100

    Delete virtualbi directory

--- virtualbi/menu-hamburguer.js
+++
@@ -1,5 +0,0 @@
-const hamburguer = document.querySelector("#id-b-hamburguer")
-
-const nav = document.querySelector('#id-nav-header')
-
-hamburguer.addEventListener("click", () => (nav.classList.toggle("active")));
